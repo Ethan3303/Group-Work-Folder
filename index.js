0b60289e3acdd0134b47283e800896d972b03133
Ben <159731246+BenjiNewt@users.noreply.github.com>
2025-04-10 13:52:52 +0100

    fix(home): hovering over settings cog

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function toggleSettingsDropdown() {
-    document.getElementById("settingsDropdown").classList.toggle("show");
+function toggleSettingsDropdown(open) {
+    const element = document.getElementById("settingsDropdown");
+    // if not open and element has mouse hovering over it, do not close it
+    if (!open && element.matches(':hover')) {
+        return;
+    }
+    element.classList.toggle("show", open);
 }
 
 function changeTime() {
