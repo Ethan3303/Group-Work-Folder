988988ca841e9cb4c81410e3c6ed855d5917987f
Ben <159731246+BenjiNewt@users.noreply.github.com>
2025-04-23 11:42:53 +0100

    Squashed and merged branch (not-done-testing) with the following commits:
    commit e501421cf0ea37ae04192edb955a8d0445f99805
    Author: Ben
    Date:   Wed Apr 23 11:36:24 2025 +0100
    
        refactor & tweak files
    
        - styles are more consistent
        - pages from main have been added
        - may be broken parts but that can be fixed after some testing
    
    commit e091dd22c9bc6817da6e09c9702e598ce34ea534
    Author: Ben
    Date:   Wed Apr 23 11:34:35 2025 +0100
    
        remove(files): copy of main as getting ready to merge this branch into main
    
    commit c5418fa45bbf31de3bc75f805291ee20f8288eb0
    Author: Ben
    Date:   Mon Apr 21 21:06:49 2025 +0100
    
        tweak(files): fix linking between sign up and in pages
    
    commit de0d243dc892b7a0953c38e190c3e70d029bc492
    Author: Ben
    Date:   Thu Apr 17 23:47:21 2025 +0100
    
        refactor(site): refactor the following
    
        - site now runs from single page importing and adding the necessary html instead of hrefing, this should prove to be easier to make edits to
        - due to  not using href now use url params to locate current page
        - some settings added such as text size changes and dark mode feature
    
        - still need to merge and add other changes that are on main and not here
    
    commit 4f0301c4ca134b75283601f65d32cf05e1af56e0
    Author: Ben
    Date:   Thu Apr 17 23:45:24 2025 +0100
    
        add(files): images from main
    
    commit 99085c0e1959a5d6cc8e34b834139e1489f320a9
    Author: Ben
    Date:   Thu Apr 17 23:44:56 2025 +0100
    
        add(files): a copy of current main changes to be added on top of the coming changes
    
    commit 2aaf220af03be8997fe43419dbdf95647c95eff5
    Author: Ben
    Date:   Wed Apr 16 17:58:35 2025 +0100
    
        refactor(files): again refactor the way in which the files are loaded
    
    commit 09c74ebb20119cc41bf8c5a4c6be2dddc32e5f4d
    Author: Ben
    Date:   Tue Apr 15 23:15:47 2025 +0100
    
        refactor(files): the way in which we do the html files should make it easier later down the line as will just import a boilerplate

---
+++ scripts/index.js
@@ -0,0 +1,95 @@
+// Main phone functionality, handles page loading and navigation
+// Make another file for page specific functionality, don't put it here
+
+function getCurrentPage() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return [urlParams.get("page") || "sign-in", urlParams.delete("page")];
+}
+
+const hideNavPages = ["home", "sign-in", "forgot-password", "sign-up"];
+const hideHeaderButtonPages = ["sign-in", "forgot-password", "sign-up"];
+
+function loadPage(page, extraParams = {}) {
+
+    const content = document.getElementById("content");
+    if (content.scrollTop > 0) {
+        extraParams.scrollTop = content.scrollTop;
+    }
+
+    fetch(`pages/${page}.html`)
+    .then(response => {
+        if (!response.ok) {
+            return fetch("pages/404.html").then(res => res.text());
+        }
+        return response.text();
+    })
+    .then(data => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, "text/html");
+        const contentToAdd = doc.getElementById("content-container");
+
+        const navbar = document.getElementsByClassName("nav-bar");
+        if (navbar && navbar[0]) {
+            navbar[0].style.display = hideNavPages.includes(page) ? "none" : "flex";
+        }
+
+
+        const settingsIcon = document.getElementById("settings-icon");
+        const notificationsIcon = document.getElementById("notifications-icon");
+        if (settingsIcon && notificationsIcon) {
+            settingsIcon.style.display = hideHeaderButtonPages.includes(page) ? "none" : "block";
+            notificationsIcon.style.display = hideHeaderButtonPages.includes(page) ? "none" : "block";
+        }
+
+        const allNavs = document.getElementsByClassName("nav-item");
+        for (const nav of allNavs) {
+            nav.classList.remove("active");
+        }
+
+        const nav = document.getElementById(`nav-${page}`);
+        if (nav) {
+            nav.classList.add("active");
+        }
+
+        content.innerHTML = contentToAdd.innerHTML;
+        document.getElementById("page-title").innerText = hideHeaderButtonPages.includes(page) ? "Parkly" : page.charAt(0).toUpperCase() + page.slice(1).replace(/-/g, " ");
+
+        const currentParams = new URLSearchParams(window.location.search);
+        if (currentParams.has("scrollTop")) {
+            content.scrollTop = parseInt(currentParams.get("scrollTop"));
+        }
+
+        const params = new URLSearchParams(extraParams).toString();
+        window.history.replaceState({ page: page }, "", `?page=${page}&${params}`);
+
+        loadSettings();
+        switch (page) {
+            case "invoice":
+                loadInvoiceData(extraParams.id);
+                break;
+            case "settings":
+                initializeSettings();
+                break;
+        }
+    });
+}
+
+function changeTime() {
+    const currentTime = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    document.getElementById("time").innerHTML = currentTime;
+}
+document.addEventListener("DOMContentLoaded", () => {
+    const [currentPage, extra] = getCurrentPage();
+    loadPage(currentPage, extra);
+    setInterval(changeTime, 10000);
+    changeTime();
+});
+
+// Add event listener for browser back/forward navigation
+window.addEventListener('popstate', (event) => {
+    if (!event.state) {
+        return;
+    }
+    const [currentPage, extra] = getCurrentPage();
+    loadPage(currentPage, extra);
+});
